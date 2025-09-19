694c552acffd5e16873c3d658226ede312504292
PURUSHOTHAMAN K <purushothk573@gmail.com>
2025-09-20 00:43:11 +0530

    Time: 1337 ms (5.05%), Space: 471 MB (8.09%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,6 +1,6 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        l,r=0,1
+        """l,r=0,1
         max_profit=0
         while r< len(prices):
             if prices[l]<prices[r]:
@@ -9,8 +9,20 @@ class Solution:
             else:
                 l=r
             r+=1
-        return max_profit
+        return max_profit"""
+        n = len(prices)
+        @lru_cache(None)
+        def f(ind, buy, cap):
+            if ind == n or cap == 0:
+                return 0
 
-        
+            if buy:
+                # either buy at this index OR skip
+                return max(-prices[ind] + f(ind + 1, 0, cap),
+                       f(ind + 1, 1, cap))
+            else:
+            # either sell at this index OR skip
+                return max(prices[ind] + f(ind + 1, 1, cap - 1),
+                       f(ind + 1, 0, cap))
 
-        
+        return f(0, 1, 1)
