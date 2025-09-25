d0e8e57ec0c1ff10e493d615e0d02ddcaa6e7c62
PURUSHOTHAMAN K <purushothk573@gmail.com>
2025-09-25 22:39:38 +0530

    Time: 3 ms (79.05%), Space: 13.2 MB (41.02%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -1,22 +1,17 @@
 class Solution(object):
     def setZeroes(self, matrix):
         rows, cols = len(matrix), len(matrix[0])
-
-        # First pass: mark cells with -1 instead of directly making them 0
+        colum_mark=[False]*cols
+        row_mark=[False]*rows
         for i in range(rows):
             for j in range(cols):
-                if matrix[i][j] == 0:
-                    # Mark entire row
-                    for k in range(cols):
-                        if matrix[i][k] != 0:
-                            matrix[i][k] = -1000
-                    for k in range(rows):
-                        if matrix[k][j] != 0:
-                            matrix[k][j] = -1000
-
+                if matrix[i][j]==0:
+                    row_mark[i]=True
+                    colum_mark[j]=True
         for i in range(rows):
             for j in range(cols):
-                if matrix[i][j] == -1000:
-                    matrix[i][j] = 0
+                if colum_mark[j] or row_mark[i]:
+                    matrix[i][j]=0
+        
 
         
