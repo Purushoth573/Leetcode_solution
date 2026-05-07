1023d8fbc53d6bf93977c41a37a7be2d22ca0ee7
PURUSHOTHAMAN K <purushothk573@gmail.com>
2026-05-07 23:11:03 +0530

    Update 0026-remove-duplicates-from-sorted-array.py

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,8 +1,26 @@
+from typing import List
+
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        l=0
-        for r in range(1,len(nums)):
-            if nums[l]!=nums[r]:
-                l+=1
-                nums[l]=nums[r]
-        return l+1
+        l = 0
+
+        for r in range(1, len(nums)):
+            if nums[l] != nums[r]:
+                l += 1
+                nums[l] = nums[r]
+
+        return l + 1
+
+
+# Input
+nums = [1, 1, 2, 2, 3, 4, 4, 5]
+
+print("Running Remove Duplicates Program")
+print("Input Array:", nums)
+
+obj = Solution()
+
+k = obj.removeDuplicates(nums)
+
+print("Number of Unique Elements:", k)
+print("Array After Removing Duplicates:", nums[:k])
