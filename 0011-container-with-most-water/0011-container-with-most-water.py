4c975aa2b6d47bf1297df1bcd84bf673da7d3995
PURUSHOTHAMAN K <purushothk573@gmail.com>
2026-05-07 23:10:24 +0530

    Update 0011-container-with-most-water.py

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,12 +1,29 @@
+from typing import List
+
 class Solution:
     def maxArea(self, height: List[int]) -> int:
         l, r = 0, len(height) - 1
         maxarea = 0
+
         while l < r:
             area = (r - l) * min(height[l], height[r])
             maxarea = max(maxarea, area)
+
             if height[l] < height[r]:
                 l += 1
             else:
                 r -= 1
+
         return maxarea
+
+
+# Input
+height = [1,8,6,2,5,4,8,3,7]
+
+print("Running Container With Most Water Program")
+print("Input Array:", height)
+
+obj = Solution()
+result = obj.maxArea(height)
+
+print("Maximum Water Container Area:", result)
