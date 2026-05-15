9c600040dfc0881df5e54882565c7c9ed8f34aa0
PURUSHOTHAMAN K <purushothk573@gmail.com>
2026-05-15 09:37:52 +0530

    Time: 10 ms (18.23%), Space: 9.3 MB (11.51%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.c
@@ -0,0 +1,12 @@
+int mySqrt(int x) {
+    long i=1;
+    while(i*i<=x){
+        i++;
+    }
+    return i-1;
+    
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
